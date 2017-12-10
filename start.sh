2730e37a0918627e2c5625805bba6601881b35a4
GregPenrose <penroseg@tcd.ie>
2017-12-10 20:31:04 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+python ./SecurityService.py
+python ./DirectoryService.py
+python ./LockService.py
+python ./FileServer1.py
+python ./FileServer2.py
+
+python ./Client.py    # Any amount of clients can now be created
